652f97698bb7c920fe47eaf5bb46724ec607f9d0
Linderval-Moura <lindervalmatias@gmail.com>
2024-09-27 19:01:26 -0300

    Organizando padrão Facade

--- ToolsLocadora/Facade/AluguelFacade.java
+++ ToolsLocadora/Facade/AluguelFacade.java
@@ -1,16 +1,62 @@
 package ToolsLocadora.Facade;
 
 import ToolsLocadora.Ferramenta;
+import ToolsLocadora.Decorator.AluguelComponente;
+import ToolsLocadora.Decorator.SeguroDecorator;
+import ToolsLocadora.Decorator.AccessorioDecorator;
+import ToolsLocadora.Memento.SalvaAluguel;
 import ToolsLocadora.Strategy.PrecoStrategy;
 
 public class AluguelFacade {
-	private PrecoStrategy strategy;
+	
+    private PrecoStrategy strategy;
+    private SalvaAluguel memento;
 
     public AluguelFacade(PrecoStrategy strategy) {
         this.strategy = strategy;
+        this.memento = new SalvaAluguel();
     }
 
-    public double rentTool(Ferramenta ferramenta, int dias) {
+    // Método para criar um aluguel
+    public AluguelComponente criarAluguel(Ferramenta ferramenta, int dias) {
+        AluguelComponente aluguel = new AluguelComponente(ferramenta, dias);
+        System.out.println(aluguel.getDescricao() + " -> Custo: R$" + aluguel.getCusto());
+        return aluguel;
+    }
+
+    // Método para adicionar seguro
+    public AluguelComponente adicionarSeguro(AluguelComponente aluguel) {
+        aluguel = new SeguroDecorator(aluguel);
+        System.out.println(aluguel.getDescricao() + " -> Custo: R$" + aluguel.getCusto());
+        return aluguel;
+    }
+
+    // Método para adicionar acessórios
+    public AluguelComponente adicionarAcessorio(AluguelComponente aluguel) {
+        aluguel = new AccessorioDecorator(aluguel);
+        System.out.println(aluguel.getDescricao() + " -> Custo: R$" + aluguel.getCusto());
+        return aluguel;
+    }
+
+    // Método para salvar o estado do aluguel
+    public void salvarEstado(AluguelComponente aluguel) {
+        memento.salvarEstado(aluguel);
+        System.out.println("Estado do aluguel salvo.");
+    }
+
+    // Método para restaurar o estado do aluguel
+    public AluguelComponente restaurarEstado() {
+        AluguelComponente estadoRestaurado = memento.restaurarEstado();
+        System.out.println(
+        		"Estado restaurado: " 
+        		+ estadoRestaurado.getDescricao()
+        		+ " -> Custo: R$" + estadoRestaurado.getCusto()
+        		);
+        return estadoRestaurado;
+    }
+
+    // Método para calcular o preço usando o Strategy
+    public double calcularPreco(Ferramenta ferramenta, int dias) {
         return strategy.calculatePrice(ferramenta, dias);
     }
 }
--- ToolsLocadora/ToolsLocadoraApp.java
+++ ToolsLocadora/ToolsLocadoraApp.java
@@ -1,106 +1,37 @@
 package ToolsLocadora;
 
-import ToolsLocadora.Decorator.AccessorioDecorator;
-import ToolsLocadora.Decorator.AluguelComponente;
-import ToolsLocadora.Decorator.SeguroDecorator;
 import ToolsLocadora.Facade.AluguelFacade;
-import ToolsLocadora.Memento.SalvaAluguel;
 import ToolsLocadora.Strategy.CurtoPrazoStrategy;
 import ToolsLocadora.Strategy.LongoPrazoStrategy;
 
 public class ToolsLocadoraApp {
 
-	public static void main(String[] args) {
-		// Ferramenta para aluguel
-		System.out.println("# Aluguel Furadeira #");
-		Ferramenta furadeira = new Ferramenta("Furadeira", 20.0);
-		
-		// Aluguel básico        
- 		AluguelComponente aluguel1 = new AluguelComponente(furadeira, 5);
- 		System.out.println(aluguel1.getDescricao() + " -> Custo: R$" + aluguel1.getCusto());
-
- 		// Adicionando seguro        
- 		aluguel1 = new SeguroDecorator(aluguel1);     
-        System.out.println(aluguel1.getDescricao() + " -> Custo: R$" + aluguel1.getCusto());
+    public static void main(String[] args) {
+        /* Ferramenta para aluguel */
+        System.out.println("# Aluguel Furadeira #");
+        Ferramenta furadeira = new Ferramenta("Furadeira", 20.0);
         
-        // Adicionando acessórios        
-        aluguel1 = new AccessorioDecorator(aluguel1);     
-        System.out.println(aluguel1.getDescricao() + " -> Custo: R$" + aluguel1.getCusto());
+        // Usando a fachada para o processo completo de aluguel
+        AluguelFacade aluguelFacade = new AluguelFacade(new CurtoPrazoStrategy());
+        var aluguelFuradeira = aluguelFacade.criarAluguel(furadeira, 5);
         
-        // Usando Strategy de preços (curto prazo)
-        AluguelFacade AluguelFacade = new AluguelFacade(new CurtoPrazoStrategy());
-        double custoTotal = AluguelFacade.rentTool(furadeira, 8);
-        System.out.println("Custo total com (Curto prazo) por 8 dias: R$" + custoTotal);
+        // Adicionando seguro e acessórios
+        aluguelFuradeira = aluguelFacade.adicionarSeguro(aluguelFuradeira);
+        aluguelFuradeira = aluguelFacade.adicionarAcessorio(aluguelFuradeira);
 
-        // Usando Strategy de preços (longo prazo)por mais de 7 dias, 10% de desconto para alugueis longos!
-        AluguelFacade AluguelFacade2 = new AluguelFacade(new LongoPrazoStrategy());
-        double custoTotal3 = AluguelFacade2.rentTool(furadeira, 8);
-        System.out.println("Custo total com (Longo prazo) por 8 dias, 10% de desconto: R$" + custoTotal3);
+        // Salvando o estado
+        aluguelFacade.salvarEstado(aluguelFuradeira);
         
-        // Salvando estado do aluguel (Memento)
-        SalvaAluguel salvaAluguel = new SalvaAluguel();
-        salvaAluguel.salvarEstado(aluguel1);
-        System.out.println("Estado Salvo!");
+        // Restaurando o estado
+        aluguelFuradeira = aluguelFacade.restaurarEstado();
+
+        // Calculando o preço com a estratégia de curto prazo
+        double custoTotal = aluguelFacade.calcularPreco(furadeira, 5);
+        System.out.println("Custo total (Curto prazo) por 5 dias: R$" + custoTotal);
         
-        // Aluguel básico
-        System.out.println("\n" + aluguel1.getDescricao() + " -> Custo: R$" + aluguel1.getCusto()+"\n");        
-
-        // Restaurando estado do aluguel
-        AluguelComponente restauraAluguel = salvaAluguel.restaurarEstado();
-        System.out.println("Restalrado: " + restauraAluguel.getDescricao() + " -> Custo: R$" + restauraAluguel.getCusto());
-
-        
-    	// Ferramenta para aluguel
-        System.out.println("\n# Aluguel Serra PoliCorte #");
- 		Ferramenta serraPoliCorte = new Ferramenta("Serra PoliCorte", 100.0);
- 		
- 		// Aluguel básico
-        AluguelComponente aluguelSerra = new AluguelComponente(serraPoliCorte, 5);
-        System.out.println(aluguelSerra.getDescricao() + " -> Custo: R$" + aluguelSerra.getCusto());
-        
-        // Adicionando seguro
-        aluguelSerra = new SeguroDecorator(aluguelSerra);
-        System.out.println(aluguelSerra.getDescricao() + " -> Custo: R$" + aluguelSerra.getCusto());
-
-        // Adicionando acessório
-        aluguelSerra = new AccessorioDecorator(aluguelSerra);
-        System.out.println(aluguelSerra.getDescricao() + " -> Custo: R$" + aluguelSerra.getCusto());
-
-        // Usando estratégia de preços (curto prazo)
-        AluguelFacade aluguelFacadeSerra = new AluguelFacade(new CurtoPrazoStrategy());
-        double custoTotalSerra = aluguelFacadeSerra.rentTool(serraPoliCorte, 5);
-        System.out.println("Custo total com estratégia (Curto prazo) por 5 dias: R$" + custoTotalSerra);
-	
-              
-        // Ferramenta para aluguel
-        System.out.println("\n# Aluguel Betoneira #");        
- 		Ferramenta betoneira = new Ferramenta("Serra Betoneira", 170.0);
- 		
- 		// Aluguel básico        
- 		AluguelComponente aluguelBetoneira = new AluguelComponente(betoneira,7);
-        System.out.println(aluguelBetoneira.getDescricao() + " -> Custo: R$" + aluguelBetoneira.getCusto());
-
-        // Adicionando seguro        
-        aluguelBetoneira= new SeguroDecorator(aluguelBetoneira);     
-        System.out.println(aluguelBetoneira.getDescricao() + " -> Custo: R$" + aluguelBetoneira.getCusto());
-        
-        // Adicionando acessório        
-        aluguelBetoneira = new AccessorioDecorator(aluguelBetoneira);     
-        System.out.println(aluguelBetoneira.getDescricao() + " -> Custo: R$" + aluguelBetoneira.getCusto());
-       
-        // Usando Strategy de preços (curto prazo)
-        AluguelFacade aluguelBetoneiraFacade = new AluguelFacade(new CurtoPrazoStrategy());
-        double custoBetoneiraTotal = aluguelBetoneiraFacade.rentTool(furadeira, 8);
-        System.out.println("Custo total com (Curto prazo) por 8 dias: R$" + custoBetoneiraTotal);
-
-        // Usando Strategy de preços (longo prazo)por mais de 7 dias, 10% de desconto para alugueis longos!
-        AluguelFacade aluguelBetoneiraFacade2 = new AluguelFacade(new LongoPrazoStrategy());
-        double custoBetoneiraTotal2 = aluguelBetoneiraFacade2.rentTool(furadeira, 8);
-        System.out.println("Custo total com (Longo prazo) por 8 dias, 10% de desconto: R$" + custoBetoneiraTotal2);
-        
-        // Salvando estado do aluguel (Memento)
-        SalvaAluguel salvaAluguelBetoneira = new SalvaAluguel();
-        salvaAluguelBetoneira.salvarEstado(aluguelBetoneira);
-        System.out.println("Estado Salvo!");
-	}
+        // Usando estratégia de preços para longo prazo
+        AluguelFacade aluguelFacadeLongoPrazo = new AluguelFacade(new LongoPrazoStrategy());
+        double custoTotalLongoPrazo = aluguelFacadeLongoPrazo.calcularPreco(furadeira, 10);
+        System.out.println("Custo total (Longo prazo) por 10 dias com 10% de desconto: R$" + custoTotalLongoPrazo);
+    }
 }
